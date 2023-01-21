a9790c260b14c012763979c8459ac030f82d7250
murkney <36377174+murkney@users.noreply.github.com>
2023-01-21 13:35:09 +0100

    Added JavaScript function for mobile navigation

--- js/modules/navigation.js
+++ js/modules/navigation.js
@@ -1,3 +1,12 @@
 export default function Navigation() {
+	const menu = document.querySelector('#mobile-navigation');
+	const menuLinks = document.querySelector('.aside__navigation');
 
-}
+
+	menu.addEventListener('click', toggleNavbar);
+
+	function toggleNavbar() {
+		menu.classList.toggle('open');
+		menuLinks.classList.toggle('active');
+	};
+}	
