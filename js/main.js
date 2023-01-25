0d94bf3bf7ad7b4a9fb7cff06e3c4c844580151b
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-01-25 15:56:08 +0100

    Imported Seach module

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import Navigation from './modules/navigation.js';
 import Subscribe from './modules/subscribe.js';
 import Events from './modules/events.js';
+import Search from './modules/search.js';
 
 Navigation();
 Subscribe();
 Events();
+Search();
