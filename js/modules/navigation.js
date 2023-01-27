a1f003684bcb1278a5378c9732a8999c1b431b75
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-01-27 13:00:15 +0100

    Feature/sticky navigation (#61)
    * Changed background color on element
    
    * Made navigation bar sticky
    
    * Adjusted font size variables, commented previous (#57)
    
    * Adjusted font size variables, commented previous (#59)
    
    * Feature/image hover mobile (#58)
    
    * Moved second image
    
    * Added text mobile
    
    * Changed variables
    
    * Changed image sizes
    
    * Feature/hover font size (#60)
    
    * Adjusted text variables and cleaned up code
    
    * Adjusted margin on hover text and cleaned up code
    
    * Changed threshold where navigation gets sticky
    
    * Changed z-index and position
    
    Co-authored-by: MarkusVaa <36377174+murkney@users.noreply.github.com>
    Co-authored-by: Sergei_ravinski <100072275+SergeiRavinski@users.noreply.github.com>

--- js/modules/navigation.js
+++ js/modules/navigation.js
@@ -1,15 +1,24 @@
 export default function Navigation() {
 	//model
 	let isVisible = false;
+
 	
 	//query selectors
 	const menu = document.querySelector('#mobile-navigation');
 	const navigationLinks = document.querySelector('.aside__navigation');
+	const navigationBar = document.querySelector('.aside__title');
 	const hamburgerIconLines = document.querySelectorAll('.hamburger-icon-line');
+	const asideLogo = document.querySelector('.aside__logo');
+
+	let navigationFixedThreshold = asideLogo.getBoundingClientRect().bottom;
 
 	//event listener
 	if (menu) {
 		menu.addEventListener('click', handleToggleNavigation);
+	}
+
+	if (navigationBar) {
+		window.addEventListener('scroll', handleNavigationScroll);
 	}
 	
 	//handler
@@ -17,6 +26,10 @@ export default function Navigation() {
 		toggleVisibility();
 		renderHTML();
 	};
+
+	function handleNavigationScroll() {
+		renderHTML();
+	}
 
 	//methods
 	function toggleVisibility() {
@@ -39,5 +52,18 @@ export default function Navigation() {
 			}
 		}
 
+		const scrollY = window.scrollY;		
+		
+		if (isVisible === true && scrollY >= navigationFixedThreshold) {
+			navigationLinks.classList.add('aside__navigation--fixed');
+		} else {
+			navigationLinks.classList.remove('aside__navigation--fixed');
+		}
+
+		if (scrollY >= navigationFixedThreshold) {
+			navigationBar.classList.add('aside__title--fixed');
+		} else {
+			navigationBar.classList.remove('aside__title--fixed');
+		}
 	}
 }	
